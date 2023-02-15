ce9d9cf8bcd0f4ab1827aa6a691f52493b26fb46
FadlyMuhammad10 <davit9747@gmail.com>
2023-02-15 21:08:01 +0700

    CRUD nominal

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const flash = require("connect-flash");
 
 var dashboardRouter = require("./app/dashboard/router");
 var categoryRouter = require("./app/category/router");
+var nominalRouter = require("./app/nominal/router");
 
 var app = express();
 
@@ -38,6 +39,7 @@ app.use(
 
 app.use("/", dashboardRouter);
 app.use("/category", categoryRouter);
+app.use("/nominal", nominalRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
