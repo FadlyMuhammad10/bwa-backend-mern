fa2ba9993659686352966da6c72496f4ab77d5b6
FadlyMuhammad10 <davit9747@gmail.com>
2023-02-15 12:19:23 +0700

    CRUD kategori & Notifikasi

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,20 +3,28 @@ const Category = require("./model");
 module.exports = {
   index: async (req, res) => {
     try {
+      const alertMessage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMessage, status: alertStatus };
       const category = await Category.find();
 
       res.render("admin/category/view_category", {
         category,
+        alert,
       });
     } catch (error) {
-      console.log(error);
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/category");
     }
   },
   viewCreate: async (req, res) => {
     try {
       res.render("admin/category/create");
     } catch (error) {
-      console.log(error);
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/category");
     }
   },
   actionCreate: async (req, res) => {
@@ -26,9 +34,61 @@ module.exports = {
       let category = await Category({ name });
       await category.save();
 
+      req.flash("alertMessage", "Berhasil tambah kategori");
+      req.flash("alertStatus", "success");
+
       res.redirect("/category");
     } catch (error) {
-      console.log(error);
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/category");
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const category = await Category.findOne({ _id: id });
+      res.render("admin/category/edit", {
+        category,
+      });
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/category");
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+
+      await Category.findOneAndUpdate({ _id: id }, { name });
+
+      req.flash("alertMessage", "Berhasil ubah kategori");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/category");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/category");
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      await Category.findOneAndRemove({ _id: id });
+
+      req.flash("alertMessage", "Berhasil hapus kategori");
+      req.flash("alertStatus", "success");
+
+      res.redirect("/category");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", `danger`);
+      res.redirect("/category");
     }
   },
 };
