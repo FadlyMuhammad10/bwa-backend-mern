d84cd6cb1f916a16677289643eebb35172cf4702
FadlyMuhammad10 <davit9747@gmail.com>
2023-02-20 20:43:17 +0700

    add image with multer

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const flash = require("connect-flash");
 var dashboardRouter = require("./app/dashboard/router");
 var categoryRouter = require("./app/category/router");
 var nominalRouter = require("./app/nominal/router");
+var voucherRouter = require("./app/voucher/router");
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(
 app.use("/", dashboardRouter);
 app.use("/category", categoryRouter);
 app.use("/nominal", nominalRouter);
+app.use("/voucher", voucherRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
